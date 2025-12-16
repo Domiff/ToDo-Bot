344bbbd7b6f4c41a2d2b7814391ee466e5bd914d
dmitriy levykin <dmlegasy@gmail.com>
2025-12-16 16:54:34 +0300

    Add models

---
+++ backend/todo/models.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Category(models.Model):
+    class UrgencyTypes(models.TextChoices):
+        PRIMARY = "Primary"
+        SECONDARY = "Secondary"
+
+    urgency = models.CharField(
+        choices=UrgencyTypes.choices,
+        default=UrgencyTypes.PRIMARY,
+    )
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.urgency
+
+
+class Task(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deadline = models.DateTimeField()
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="tasks"
+    )
+
+    class Meta:
+        ordering = ["-created_at"]
+        indexes = [
+            models.Index(fields=["-created_at"]),
+        ]
+
+    def __str__(self):
+        return self.title
