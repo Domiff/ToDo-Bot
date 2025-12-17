3043b4ba6a5d497805acc79de9c6393bbe336582
dmitriy levykin <dmlegasy@gmail.com>
2025-12-17 15:00:32 +0300

    Renamed env file

--- backend/todo_manager/config.py
+++ backend/todo_manager/config.py
@@ -11,7 +11,7 @@ class Settings(BaseSettings):
     POSTGRES_HOST: str
     POSTGRES_POST: int
 
-    model_config = SettingsConfigDict(env_file="../.env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(env_file="../.env.django", env_file_encoding="utf-8")
 
 
 settings = Settings()
