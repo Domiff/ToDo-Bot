2f4585c45dc3cce6b08f25490a0766653786b219
dmitriy levykin <dmlegasy@gmail.com>
2025-12-21 20:26:38 +0300

    Add function for update task

---
+++ bot/crud/update.py
@@ -0,0 +1,13 @@
+from bot.api import ApiClient, urls_dict
+from bot.crud.utils import check_token
+
+
+async def update_task(access, refresh, state, task, pk):
+    headers = {"Authorization": f"Bearer {access}"}
+    async with ApiClient(urls_dict.get("base_url")) as client:
+        data = await client.patch(urls_dict.get("update"), headers=headers, data=task, pk=pk)
+        code = data[1]
+        if code == 401:
+            headers = await check_token(refresh, state)
+            data = await client.patch(urls_dict.get("update"), headers=headers, data=task, pk=pk)
+        return data
