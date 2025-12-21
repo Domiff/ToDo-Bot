78dd1eba32b0cfaa41a99a96e7e9cbfd175f06c0
dmitriy levykin <dmlegasy@gmail.com>
2025-12-21 21:52:27 +0300

    Included dialog in router for registration

--- bot/dialogs/registration/router.py
+++ bot/dialogs/registration/router.py
@@ -5,9 +5,11 @@ from aiogram.types import Message
 from aiogram_dialog import DialogManager, StartMode
 
 from .states import RegistrationState
+from .dialog import register_dialog
 
 
 router = Router()
+router.include_router(register_dialog)
 
 
 @router.message(CommandStart())
