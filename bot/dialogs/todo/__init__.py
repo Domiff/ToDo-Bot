541de0e931c8d25053e473b4c2f8a1a959645f01
dmitriy levykin <dmlegasy@gmail.com>
2025-12-21 20:34:13 +0300

    Add __init.py__ from todo

---
+++ bot/dialogs/todo/__init__.py
@@ -0,0 +1,7 @@
+from .read import read_dialog
+from .create import create_dialog
+from .update import update_dialog
+from .router import router as todo_router
+
+
+__all__ = ["todo_router", "read_dialog", "create_dialog", "update_dialog"]
