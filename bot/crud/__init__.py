295b0ff744d07cf5c97d94845a88f1684101968c
dmitriy levykin <dmlegasy@gmail.com>
2025-12-21 20:27:22 +0300

    Add utils for crud operations

---
+++ bot/crud/__init__.py
@@ -0,0 +1,6 @@
+from .read import prepare_message, get_tasks
+from .create import create_task
+from .update import update_task
+
+
+__all__ = ["prepare_message", "create_task", "get_tasks", "update_task"]
