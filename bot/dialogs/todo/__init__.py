09d0107aeb32961c226c8f53739b89dbeea8781e
dmitriy levykin <dmlegasy@gmail.com>
2025-12-21 21:54:10 +0300

    Added dialog for delete task

--- bot/dialogs/todo/__init__.py
+++ bot/dialogs/todo/__init__.py
@@ -1,7 +1,8 @@
 from .read import read_dialog
 from .create import create_dialog
 from .update import update_dialog
+from .delete import delete_dialog
 from .router import router as todo_router
 
 
-__all__ = ["todo_router", "read_dialog", "create_dialog", "update_dialog"]
+__all__ = ["todo_router", "read_dialog", "create_dialog", "update_dialog", "delete_dialog"]
--- bot/dialogs/todo/router.py
+++ bot/dialogs/todo/router.py
@@ -3,7 +3,8 @@ from aiogram import Router
 from .read import read_router
 from .create import create_router
 from .update import update_router
+from .delete import delete_router
 
 
 router = Router()
-router.include_routers(read_router, create_router, update_router)
+router.include_routers(read_router, create_router, update_router, delete_router)
