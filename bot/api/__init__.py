4f99719701d0ed065af7e256dc8ef903f8bd5728
dmitriy levykin <dmlegasy@gmail.com>
2025-12-21 20:23:19 +0300

    Add auth client and add headers for methods in api client

--- bot/api/__init__.py
+++ bot/api/__init__.py
@@ -1,5 +1,5 @@
-from .client import ApiClient
+from .client import ApiClient, AuthClient
 from .urls import urls_dict
 
 
-__all__ = "ApiClient", "urls_dict"
+__all__ = "ApiClient", "AuthClient", "urls_dict"
--- bot/api/client.py
+++ bot/api/client.py
@@ -14,27 +14,33 @@ class Client:
 
 
 class ApiClient(Client):
-    async def get(self, url: str, pk: int = None):
+    async def get(self, url: str, headers: dict, pk: int = None):
         if pk:
             url += str(pk)
-        async with self.session.get(url) as response:
+        async with self.session.get(url, headers=headers) as response:
             return await response.json(), response.status
 
-    async def post(self, url: str, data: dict):
-        async with self.session.post(url, data=data) as response:
+    async def post(self, url: str, data: dict, headers: dict):
+        async with self.session.post(url, data=data, headers=headers) as response:
             return await response.json(), response.status
 
-    async def put(self, url: str, pk: int, data: dict):
+    async def put(self, url: str, pk: int, data: dict, headers: dict):
         url += str(pk)
-        async with self.session.put(url, data=data) as response:
+        async with self.session.put(url, data=data, headers=headers) as response:
             return await response.json(), response.status
 
-    async def patch(self, url: str, pk: int, data: dict):
+    async def patch(self, url: str, pk: int, data: dict, headers: dict):
         url += str(pk)
-        async with self.session.patch(url, data=data) as response:
+        async with self.session.patch(url, data=data, headers=headers) as response:
             return await response.json(), response.status
 
-    async def delete(self, url: str, pk):
+    async def delete(self, url: str, pk, headers: dict):
         url += str(pk)
-        async with self.session.delete(url) as response:
+        async with self.session.delete(url, headers=headers) as response:
             return response.status
+
+
+class AuthClient(Client):
+    async def post(self, url: str, payload: dict):
+        async with self.session.post(url, json=payload) as client:
+            return await client.json()
