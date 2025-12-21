67e9a495b10119114d2408c47e1f07aed6a85f8e
dmitriy levykin <dmlegasy@gmail.com>
2025-12-21 20:30:58 +0300

    Add dialog for show tasks

---
+++ bot/dialogs/todo/read/__init__.py
@@ -0,0 +1,5 @@
+from .dialog import read_dialog
+from .router import router as read_router
+
+
+__all__ = ["read_router", "read_dialog"]
---
+++ bot/dialogs/todo/read/handlers.py
@@ -0,0 +1,17 @@
+from aiogram.fsm.context import FSMContext
+from aiogram.types import CallbackQuery
+from aiogram_dialog import DialogManager
+
+from bot.crud import prepare_message
+
+
+async def get_tasks_handler(
+    callback: CallbackQuery, button, dialog_manager: DialogManager)-> None:
+    state: FSMContext = dialog_manager.middleware_data["state"]
+    data = await state.get_data()
+    access = data.get("access")
+    refresh = data.get("refresh")
+    tasks = await prepare_message(access, refresh, state)
+    for task in tasks:
+        await callback.message.answer(task)
+    await dialog_manager.done()
