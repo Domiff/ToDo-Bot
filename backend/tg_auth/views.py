aec73cafdb8e8055f69dd3ea0fb084a6a9a713d7
dmitriy levykin <dmlegasy@gmail.com>
2025-12-21 20:15:06 +0300

    Add permision AllowAny for register view

--- backend/tg_auth/views.py
+++ backend/tg_auth/views.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
 from .serializers import ProfileSerializer
@@ -6,6 +7,7 @@ from .serializers import ProfileSerializer
 
 class RegisterAPIView(CreateAPIView):
     serializer_class = ProfileSerializer
+    permission_classes = AllowAny,
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
