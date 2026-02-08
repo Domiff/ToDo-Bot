be73fd300f8d84d4e6458e1eead9198283b826b8
dmitriy levykin <dmlegasy@gmail.com>
2026-02-08 21:05:39 +0300

    Add async for tg_auth views

--- backend/tg_auth/views.py
+++ backend/tg_auth/views.py
@@ -1,6 +1,7 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+from adrf.generics import CreateAPIView
 
 from .serializers import ProfileSerializer
 
@@ -9,8 +10,9 @@ class RegisterAPIView(CreateAPIView):
     serializer_class = ProfileSerializer
     permission_classes = AllowAny,
 
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
+    async def create(self, request, *args, **kwargs):
+        serializer = ProfileSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
-        return Response(serializer.to_representation(user))
+        data = await serializer.ato_representation(user)
+        return Response(data, status=status.HTTP_201_CREATED)
